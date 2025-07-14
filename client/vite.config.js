7c8e57c7247844c90d990e2e42edb7d4d3606214
odingaval <odingaval71@gmail.com>
2025-07-14 20:54:51 +0300

    overhaul configuration for production build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
-      '/uploads': 'http://localhost:5000'
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+      '/uploads': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+      '/socket.io': {  
+        target: 'http://localhost:5000',
+        ws: true,      
+        changeOrigin: true,
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['socket.io-client'],  
+    exclude: [],                   
+  },
+  build: {
+    commonjsOptions: {
+      include: [/socket.io-client/, /node_modules/],  // Handle CJS modules
+    },
+    rollupOptions: {
+      external: [], 
     }
   }
-})
+});
